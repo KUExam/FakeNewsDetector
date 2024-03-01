f37948ea12ce8506b99962ea773f4ff3adfa0ae7
Gustav Jes Iversen <gustavjes0105@gmail.com>
2024-03-01 11:36:55 +0100

    fasfds

--- Part 1/task_1.py
+++ Part 1/task_1.py
@@ -1,33 +1,38 @@
-import csv
 import re
 from cleantext import clean
 
 def clean_text_with_cleantext(text):
     date_pattern = r'\b(?:\d{1,2}[-/]\d{1,2}[-/]\d{2,4})\b'
     text = re.sub(date_pattern, '<DATE>', text)
-    cleaned_text = clean(text,
-                         fix_unicode=True,               # fix various unicode errors
-                         to_ascii=True,                  # transliterate to closest ASCII representation
-                         lower=True,                     # lowercase text
-                         no_line_breaks=True,            # fully strip line breaks as opposed to only normalizing them
-                         normalize_whitespace=True,           
-                         no_urls=True,                   # replace all URLs with a special token
-                         no_emails=True,                 # replace all email addresses with a special token
-                         no_numbers=True,                # replace all numbers with a special token
-                         no_punct=True,                  # remove punctuations
-                         replace_with_url="<URL>",       # relpacing URLS with <URL>
-                         replace_with_email="<EMAIL>",   # replacing emails with <EMAIL>
-                         replace_with_number="<NUM>",
-                         lang="en"),
-    
+    cleaned_text = clean(
+        text,
+        fix_unicode=True,               # fix various unicode errors
+        to_ascii=True,                  # transliterate to closest ASCII representation
+        lower=True,                     # lowercase text
+        normalize_whitespace=True,      # Removes multiple whitespaces
+        no_line_breaks=True,            # fully strip line breaks as opposed to only normalizing them
+        strip_lines=True,               # 
+        keep_two_line_breaks=False,     #
+        no_urls=True,                   # replace all URLs with a special token
+        no_emails=True,                 # replace all email addresses with a special token
+        no_numbers=True,                # replace all numbers with a special token
+        no_digits=False,                # replace all digits with a special token
+        no_currency_symbols=False,      # replace all currency symbols with a special token
+        no_punct=False,                 # remove punctuations
+        no_emoji=True,                  # Emoji remover
+        replace_with_url="<URL>",
+        replace_with_email="<EMAIL>",
+        replace_with_number="<NUM>",
+        replace_with_punct="",
+        lang="en"                       # Language, set to 'en' for English special handling
+    )
 
     return cleaned_text
 
-
 import pandas as pd
 import numpy as np
-url='https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv'
-df = pd.read_csv(url)
+url='https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv?raw=true'
+df = pd.read_csv(url, index_col=0)
 
 df['content']=df['content'].apply(clean_text_with_cleantext)
 df.head()
